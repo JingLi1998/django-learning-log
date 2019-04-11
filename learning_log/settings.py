3b7680a77992a7cda2da117c62f0bda35ebe6b46
Jing Li <jing_li1998@hotmail.com>
2019-04-11 23:23:31 +1000

    new

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -42,7 +42,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     DATABASES = {
         'default': dj_database_url.config(default='postgres://localhost')
     }
-
+    
     # Honour the 'X-Forwarded-Proto' header for request.is_secure()
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
